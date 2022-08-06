921a427d9f4a257876910dedc57f6e061fe4416e
Felix J <felix.hannes.fj@gmail.com>
2022-08-06 22:57:52 +0200

    merge conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
 import { HttpModule } from "@nestjs/axios";
 import { ScheduleModule } from "@nestjs/schedule";
 import { ConfigModule } from "@nestjs/config";
-import { PrismaModule } from './prisma/prisma.module';
-import { FitxService } from './fitx/fitx.service';
-import { TimeService } from "./time/time.service";
-import { HealthModule } from './health/health.module';
+import { PrismaModule } from "./prisma/prisma.module";
+import { FitxService } from "./fitx/fitx.service";
+import { TimeService } from "./services/time/time.service";
+import { StudioHttpClientService } from "./services/studio-http-client/studio-http-client.service";
+import { DataExtractorService } from "./services/data-extractor/data-extractor.service";
+import { CronSchedulerService } from "./services/cron-scheduler/cron-scheduler.service";
 
 @Module({
   imports: [HttpModule,
-  ScheduleModule.forRoot(), ConfigModule.forRoot(), PrismaModule, HealthModule],
+    ScheduleModule.forRoot(), ConfigModule.forRoot(), PrismaModule],
   controllers: [AppController],
-  providers: [FitxService, TimeService],
+  providers: [FitxService, TimeService, StudioHttpClientService, DataExtractorService, CronSchedulerService]
 })
-export class AppModule {}
+export class AppModule {
+}
